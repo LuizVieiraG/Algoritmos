10db0f78c92934154997e19194340a137d6d14f9
LuizVieiraG <131825692+LuizVieiraG@users.noreply.github.com>
2023-05-31 00:13:50 -0300

    att exec 1

--- Lista 19/Exercicio1.c
+++ Lista 19/Exercicio1.c
@@ -10,19 +10,22 @@ imprima:
 - a quantidade de homens.*/
 
 void main(){
-	int i;
-	float menor_alt, maior_alt, altura;
+	int i, sexo, qtde_homens = 0, cont_altf = 0;
+	float menor_alt, maior_alt, altura, soma_alt_turma = 0; 
+	float media_alt_mulheres = 0, media_alt_turma = 0;
 	
-	for(i=1; i <= 3; i++){
-		printf("Informe Sua altura: ");
+	//valores Iniciais de Variaveis
+	maior_alt = 0;
+	menor_alt = 99999;
+	
+	for(i=1; i <= 10; i++){
+		printf("\nFicha: %d", i);
+		printf("\nInforme Sua altura: ");
 		scanf("%f", &altura);
-		printf("Informe seu Sexo (1-Masculino,2-Feminino): ");
-		scanf("");
-		if(i == 1){
-			maior_alt = altura;
-			menor_alt = altura;
-		}
+		printf("\nInforme seu Sexo (1-Masculino,2-Feminino): ");
+		scanf("%d", &sexo);
 		
+		//verificar Maior e menor altura
 		if(altura > maior_alt){
 			maior_alt = altura;	
 		}
@@ -31,8 +34,28 @@ void main(){
 			menor_alt = altura;		
 		}
 		
+		// Verificando se o sexo é feminino e calculando a soma das alturas das mulheres
+		if(sexo == 2){
+			media_alt_mulheres += altura;
+			cont_altf++;
+		}
+		
+		//// Calcular a soma da altura da turma
+		soma_alt_turma += altura;
+		
+		// Verificando se o sexo é masculino
+		if(sexo == 1){
+			qtde_homens++;
+		}
 	}
+	// Calculando as médias
+	media_alt_mulheres /= cont_altf;
+	media_alt_turma = soma_alt_turma / 10;
+	
 	printf("\nMaior Altura: %0.2f", maior_alt);
 	printf("\nMenor Altura: %0.2f", menor_alt);
+	printf("\nMedia de Altura das Mulheres: %0.2f", media_alt_mulheres);
+	printf("\nMedia de Altura da Turma: %0.2f", media_alt_turma);
+	printf("\nQuantidade de Homens: %d", qtde_homens);
 }
 
