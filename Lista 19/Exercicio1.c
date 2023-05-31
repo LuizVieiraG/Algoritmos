5b6eb2d603549eb25857f295b5b055d4ae389070
LuizVieiraG <luizvieirag12@gmail.com>
2023-05-30 22:07:49 -0300

    att

--- Lista 19/Exercicio1.c
+++ Lista 19/Exercicio1.c
@@ -10,6 +10,29 @@ imprima:
 - a quantidade de homens.*/
 
 void main(){
+	int i;
+	float menor_alt, maior_alt, altura;
 	
+	for(i=1; i <= 3; i++){
+		printf("Informe Sua altura: ");
+		scanf("%f", &altura);
+		printf("Informe seu Sexo (1-Masculino,2-Feminino): ");
+		scanf("");
+		if(i == 1){
+			maior_alt = altura;
+			menor_alt = altura;
+		}
+		
+		if(altura > maior_alt){
+			maior_alt = altura;	
+		}
+		
+		if(altura < menor_alt){
+			menor_alt = altura;		
+		}
+		
+	}
+	printf("\nMaior Altura: %0.2f", maior_alt);
+	printf("\nMenor Altura: %0.2f", menor_alt);
 }
 
