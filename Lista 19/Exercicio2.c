4b6f0982e75fd2a0a0c8a56f38e121f6e28c4f21
LuizVieiraG <131825692+LuizVieiraG@users.noreply.github.com>
2023-06-08 18:05:58 -0300

    att

--- Lista 19/Exercicio2.c
+++ Lista 19/Exercicio2.c
@@ -10,6 +10,29 @@ cada uma delas;
 */
 
 void main(){
-	
+	int matricula, i = 0, mat_maior = 0, segmat_maior = 0;
+	float nota, maior_nota = 0, seg_notamaior = 0; 
+	printf("\n<<Matriculas>>");
+	do{
+		printf("\nNumero da Matricula: ");
+		scanf("%d", &matricula);
+		printf("\nNota: ");
+		scanf("%f", &nota);
+		
+		if(nota > maior_nota){
+			seg_notamaior = maior_nota;
+			maior_nota = nota;
+			mat_maior = matricula;	
+		}else 
+			if(nota > seg_notamaior){
+				seg_notamaior = nota;
+				segmat_maior = matricula;
+			}
+			
+		i++;
+	}while(i < 7);
+	printf("\nAs Duas Maiores Notas: ");
+	printf("\nNumero da Matricula: %d, Nota: %.2f", mat_maior, maior_nota);
+	printf("\nNumero da Matricula: %d, Nota: %.2f", segmat_maior, seg_notamaior);
 }
 
