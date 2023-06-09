7148cdb7dad8827112c51281371f94d59868b0a1
LuizVieiraG <131825692+LuizVieiraG@users.noreply.github.com>
2023-06-09 18:44:23 -0300

    att

--- Lista 19/Exercicio3.c
+++ Lista 19/Exercicio3.c
@@ -16,7 +16,61 @@ b)	Calcule e exiba:
 "	A porcentagem de alunos reprovados por faltas.*/
 
 void main(){
-	int nota_maior = 0, nota_menor = 0, i;
-	float media, nota, 
+    int matricula, frequencia, totalReprovados = 0, totalAlunos = 10, cont = 1;
+    float nota1, nota2, nota3, notaFinal, maiorNota, menorNota, somaNotas = 0.0, percentualReprovados;
+
+    printf("<<Notas e frequência de alunos>>");
+
+    while (cont <= totalAlunos) {
+        printf("\nAlunos: %d", cont);
+        printf("\nNumero de matricula: ");
+        scanf("%d", &matricula);
+
+        printf("Nota 1: ");
+        scanf("%f", &nota1);
+
+        printf("Nota 2: ");
+        scanf("%f", &nota2);
+
+        printf("Nota 3: ");
+        scanf("%f", &nota3);
+
+        printf("Frequencia (numero de aulas frequentadas): ");
+        scanf("%d", &frequencia);
+
+        notaFinal = (nota1 + nota2 + nota3) / 3;
+
+        printf("Nota final: %.2f\n", notaFinal);
+
+        if (cont == 1) {
+            maiorNota = notaFinal;
+            menorNota = notaFinal;
+        } else {
+            if (notaFinal > maiorNota) {
+                maiorNota = notaFinal;
+            }
+            if (notaFinal < menorNota) {
+                menorNota = notaFinal;
+            }
+        }
+
+        somaNotas += notaFinal;
+
+        if (notaFinal < 6.0 || frequencia < 40) {
+            totalReprovados++;
+        }
+
+        cont++;
+    }
+
+    printf("\n<<Resultados>>");
+    printf("\nMaior nota da turma: %.2f", maiorNota);
+    printf("\nMenor nota da turma: %.2f", menorNota);
+    printf("\nNota media da turma: %.2f", somaNotas / totalAlunos);
+    printf("\nTotal de alunos reprovados: %d", totalReprovados);
+
+    percentualReprovados = (float)totalReprovados / totalAlunos * 100;
+    printf("\nPorcentagem de alunos reprovados por faltas: %.2f%%", percentualReprovados);
+
 }
 
